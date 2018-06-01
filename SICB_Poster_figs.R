c6cb57b81e11bdd13f450a13f046b288a4aa9348
aecahill <aecsk8gr8@yahoo.com>
2018-06-01 09:01:22 -0400

    changes 1 June
    Changes to incorporate a new fig with the percentage of cryptic species
    in each phylum

--- SICB_Poster_figs.R
+++ SICB_Poster_figs.R
@@ -23,6 +23,26 @@ ggplot(cs_species, aes(x = Phylum, y = total, fill=Phylum)) +
   theme(legend.position="none")+
   scale_fill_hue(c=100, l=45)
 
+#corrected for the percentage of species in worms
+
+cs_correct<-read.table("C:/Users/Abigail/Desktop/cs_percentages.txt",header=T)
+
+cs_correct$Phylum <- factor(cs_correct$Phylum, levels = cs_correct$Phylum[order(-cs_correct$Percent_CS)])
+cs_correct$Phylum  # notice the changed order of factor levels
+
+ggplot(cs_correct, aes(x = Phylum, y = Percent_CS, fill=Phylum)) + 
+  theme_bw() + 
+  theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank())+
+  geom_bar(stat = "identity")+
+  ylab("Percent CS\n")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 14))+
+  theme(axis.title.x = element_text(size = 16))+
+  theme(axis.title.y = element_text(size = 16))+
+  theme(axis.text.y = element_text(size = 14))+
+  theme(legend.position="none")+
+  scale_fill_hue(c=100, l=45)
+
+
 #what habitat are the CS in?
 
 cs_habitat<-read.table("C:/Users/acahill/Desktop/cs_habitat.txt",header=T)
