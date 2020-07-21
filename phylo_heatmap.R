98328592787c68e55d55e1ba9b131119d184b54f
aecahill <aecsk8gr8@yahoo.com>
2020-07-21 15:03:39 -0400

    changes_july21
    Changed July 21 to add proper input files and change code to color heatmap differently

--- phylo_heatmap.R
+++ phylo_heatmap.R
@@ -2,19 +2,41 @@
 
 library(phytools) 
 
+#For 58 classes, ie all cs
+
 #read in tree, newick format
-treedata<-read.tree("C:/Users/aecsk/OneDrive/Desktop/tree.tre")
+tree58data<-read.tree("C:/Users/aecsk/OneDrive/Desktop/tree58.tre")
 
 #read in matrix of continuous traits
-traits<-as.matrix(read.table("C:/Users/aecsk/OneDrive/Desktop/traits.txt"))
+traits58<-as.matrix(read.table("C:/Users/aecsk/OneDrive/Desktop/traits58.txt"))
 
 #this is a list of classes to use as row names, as required - got to be an easier way here
-classes<-read.table("C:/Users/aecsk/OneDrive/Desktop/classnames.txt")
+class58<-read.table("C:/Users/aecsk/OneDrive/Desktop/class58.txt")
 
 #rename rows and columns
-rownames(traits)<-classes$V1
-colnames(traits)<-c("Effort_m1","Ferti","Residus")
+rownames(traits58)<-class58$V1
+colnames(traits58)<-c("Effort","Fertilization","Residuals")
 
 #make figure
-phylo.heatmap(treedata,traits,standardize=T,fsize=c(0.5,1,1))
+colors<-colorRampPalette(colors=c("white","blue"))(20)
+phylo.heatmap(tree58data,traits58,standardize=T,fsize=c(0.45,1,1),colors=colors)
 
+
+#For 51 classes, ie only css
+
+#read in tree, newick format
+tree51data<-read.tree("C:/Users/aecsk/OneDrive/Desktop/tree51.tre")
+
+#read in matrix of continuous traits
+traits51<-as.matrix(read.table("C:/Users/aecsk/OneDrive/Desktop/traits51.txt"))
+
+#this is a list of classes to use as row names, as required - got to be an easier way here
+class51<-read.table("C:/Users/aecsk/OneDrive/Desktop/class51.txt")
+
+#rename rows and columns
+rownames(traits51)<-class51$V1
+colnames(traits51)<-c("Effort","Fertilization","Residuals")
+
+#make figure
+colors<-colorRampPalette(colors=c("white","blue"))(20)
+phylo.heatmap(tree51data,traits51,standardize=T,fsize=c(0.45,1,1),colors=colors)
