5a89b91b1b632e671d8d7104ea25b40555f52171
aecahill <aecsk8gr8@yahoo.com>
2021-07-08 21:48:48 -0400

    range_size
    Starting file to look at range size for species in OBIS

---
+++ rangesize.R
@@ -0,0 +1,10 @@
+
+## How to find range size in OBIS
+
+library(robis)
+
+# For each species, get occurrence data (use Mark's code here)
+# Find max and min latitude, max and min longitude
+# Find difference; convert to km
+# End goal: A table with species name, diff lat, diff long, which is bigger (lat or long)
+
