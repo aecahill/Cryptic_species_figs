e71903c43b67e59478420aecdfcfcc8f13579e25
aecahill <aecsk8gr8@yahoo.com>
2021-07-14 21:06:19 -0400

    rangesize3
    Altered code for range size, now with means and sds

--- rangesize.R
+++ rangesize.R
@@ -2,15 +2,16 @@
 ## How to find range size in OBIS
 
 library(robis)
+library(dplyr)
 
-# For each species, get occurrence data (use Mark's code here)
+# For each species, get occurrence data 
 
 ##Part 1: pulling all animal species from OBIS
 
 animalia<-checklist(taxonid=2)   #pulls list of all animals from OBIS
 animalspp<-animalia[animalia$taxonRank=="Species",]   #ONLY use those that are ID'd to the species level
 animals<-as.data.frame(cbind(animalspp$scientificName,animalspp$phylum))  #create table of names and phyla
-animals2<-na.omit(animals)
+animals2<-na.omit(animals) #removing species whose name is "NA"
 
 ## Part 2: get occurrences
 
@@ -28,7 +29,12 @@ for (i in animals2$V1) {
 
 }
 
-specieslist<-read.csv("C:/Users/acahill/Documents/GitHub/Cryptic_species_figs/specieslist.csv")
+write.csv(species_diffs,"C:/Users/aecsk/Documents/GitHub/Cryptic_species_figs/species_diffs.csv")
+
+
+## Here is where I checked to see if the species contain CS based on the survey
+
+specieslist<-read.csv("C:/Users/aecsk/Documents/GitHub/Cryptic_species_figs/specieslist.csv")
 
 species_diffstable = NULL
 in_survey = NULL
@@ -39,9 +45,36 @@ for (i in species_diffs$i) {
   
 }
 
-species_diffstable = cbind(species_diffs,is_in_survey)
+species_diffstable = cbind(species_diffs,in_survey)
 
-write.csv(species_diffs,"C:/Users/acahill/Documents/GitHub/Cryptic_species_figs/species_diffs.csv")
+write.csv(species_diffstable,"C:/Users/aecsk/Documents/GitHub/Cryptic_species_figs/species_diffstable.csv")
 
-# End goal: A table with species name, diff lat, diff long, which is bigger (lat or long)
+
+#Attempting some stats
+
+#First, remove species without any occurrence data (gave -Inf results)
+#AND removing data with no longitude variation (range = 0)
+
+#sort on longitude
+specieslong<-species_diffstable[order(species_diffstable$range_long),]
+
+#remove -INF and zero values
+#Note: I just did this by figuring out where they were in the sorted data frame; did not write code for it.
+
+specieslong_noNA<-specieslong[30616:120772,]
+
+#Mean and sd for in survey vs not
+
+tapply(as.numeric(specieslong_noNA$range_long),specieslong_noNA$in_survey,mean)
+tapply(as.numeric(specieslong_noNA$range_long),specieslong_noNA$in_survey,sd)
+
+#repeat for latitude
+
+specieslat<-species_diffstable[order(species_diffstable$range_lat),]
+specieslat_noNA<-specieslat[30801:120772,]
+
+tapply(as.numeric(specieslat_noNA$range_lat),specieslat_noNA$in_survey,mean)
+tapply(as.numeric(specieslat_noNA$range_lat),specieslat_noNA$in_survey,sd)
+
+
 
