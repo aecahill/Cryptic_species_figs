1fc67cae3f898be7d9cf3ea3ae8a25072b84c424
aecahill <aecsk8gr8@yahoo.com>
2021-07-27 15:02:57 -0400

    changes_July27
    Edits to heatmap and associated data July 27 2021

--- heatmap_separate.R
+++ heatmap_separate.R
@@ -127,3 +127,41 @@ heatmap<-ggplot(data = traits57, mapping = aes(x = Variable,
   geom_hline(yintercept=40.5)+
   geom_hline(yintercept=42.5)+
   facet_grid(~Variable,switch = "x", scales = "free_x", space = "free_x")
+
+## 58 classes, new results, July 2021
+
+tree58data<-read.tree("C:/Users/acahill/Documents/GitHub/Cryptic_species_figs/tree58.tre")
+
+tree58<-ggtree(tree58data)
+
+traits58<-read.table("C:/Users/acahill/Documents/GitHub/Cryptic_species_figs/traits_58_July27.txt",header=T)
+
+heatmap<-ggplot(data = traits58, mapping = aes(x = Variable,
+                                               y = Class_num,
+                                               fill = z_score)) +
+  geom_tile() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red')+
+  theme_bw()+
+  theme(axis.title.x = element_blank(), 
+        axis.title.y = element_blank(), 
+        panel.background = element_blank(), 
+        panel.grid.major = element_blank(),  
+        panel.grid.minor = element_blank(),  
+        plot.background = element_blank())+
+  theme(legend.position="none") +
+  theme(axis.text.x=element_blank(),axis.text.y=element_blank())+
+  geom_hline(yintercept=4.5)+
+  geom_hline(yintercept=9.5)+
+  geom_hline(yintercept=16.5)+
+  geom_hline(yintercept=17.5)+
+  geom_hline(yintercept=18.5)+
+  geom_hline(yintercept=23.5)+
+  geom_hline(yintercept=24.5)+
+  geom_hline(yintercept=25.5)+
+  geom_hline(yintercept=27.5)+
+  geom_hline(yintercept=28.5)+
+  geom_hline(yintercept=31.5)+
+  geom_hline(yintercept=39.5)+
+  geom_hline(yintercept=41.5)+
+  geom_hline(yintercept=43.5)+
+  facet_grid(~Variable,switch = "x", scales = "free_x", space = "free_x")
