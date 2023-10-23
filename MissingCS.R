b578336fe42989e2262212e1596a1efd2dea1e55
aecahill <aecsk8gr8@yahoo.com>
2023-10-23 11:16:32 -0400

    more_oceancode
    More code and files to find missing spp

--- MissingCS.R
+++ MissingCS.R
@@ -13,6 +13,12 @@ animalspp<-animalia[animalia$taxonRank=="Species",]   #ONLY use those that are I
 animals<-as.data.frame(cbind(animalspp$scientificName,animalspp$phylum,animalspp$class))  #create table of names and phyla
 
 ##Part2: need polygons for all seas - read in Mark's code
-
+Arctic_coor<-read.csv("Arctic_coor.csv")
+Atlantic_coor<-read.csv("Atlantic_coor.csv")
+Indian_coor<-read.csv("Indian_coor.csv")
+Pacific_coor<-read.csv("Pacific_coor.csv")
+SouthOcean_coor<-read.csv("SouthOcean_coor.csv")
 
 ##Part3: create checklists for each sea
+
+#need to somehow get these checklists into a single polygon or something, look at Mark's code
